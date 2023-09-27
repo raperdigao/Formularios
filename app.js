5f31094472839691ff5c9d95fc5303240b17edfc
Raphael Pagni Perdigão <110313895+raperdigao@users.noreply.github.com>
2023-09-27 11:57:47 +0000

    Atualizado Script JS

--- app.js
+++ app.js
@@ -8,15 +8,3 @@ const d = new Date();
 document.getElementById("data").innerHTML = d;
 
 
-function validaVazio() {
-  // Valida o campo Nome
-  if (document.teste.nome.value == "") {
-   window.alert("Campo Requerido");
-   document.teste.nome.focus();
-   return false;
-  } else {
-   window.alert("Dados envidao com sucesso");
-   return true;
-  }
-
-  }
